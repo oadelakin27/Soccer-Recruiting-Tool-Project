a6efb8071a26833ecee5374f8bb3720113cece4c
oadelakin27 <oadelakin27@groton.org>
2026-05-23 21:12:30 -0400

    Finished adding ACC, Big East, and Big Ten schools

--- seed_data.py
+++ seed_data.py
@@ -20,25 +20,44 @@ schools_data = [
     {"name": "Clemson University", "conference": "ACC", "state": "South Carolina", "head_coach": "Mike Noonan", "recruiting_email": "msoccer@clemson.edu", "camp_dates": "TBD"},
     {"name": "Syracuse University", "conference": "ACC", "state": "New York", "head_coach": "Ian McIntyre", "recruiting_email": "ismcinty@syr.edu", "camp_dates": "TBD"},
     {"name": "Notre Dame", "conference": "ACC", "state": "Indiana", "head_coach": "Chad Riley", "recruiting_email": "criley@nd.edu", "camp_dates": "TBD"},
+    {"name": "Boston College", "conference": "ACC", "state": "Massachusetts", "head_coach": "Bob Thompson", "recruiting_email": "msoccer@bc.edu", "camp_dates": "TBD"},
+    {"name": "University of California", "conference": "ACC", "state": "California", "head_coach": "Leonard Griffin", "recruiting_email": "calsoccer@berkeley.edu", "camp_dates": "TBD"},
+    {"name": "University of Louisville", "conference": "ACC", "state": "Kentucky", "head_coach": "John Michael Hayden", "recruiting_email": "johnh@GoCards.com", "camp_dates": "TBD"},
+    {"name": "NC State", "conference": "ACC", "state": "North Carolina", "head_coach": "Marc Hubbard", "recruiting_email": "MENS_SOCCER@NCSU.EDU", "camp_dates": "TBD"},
+    {"name": "University of Pittsburgh", "conference": "ACC", "state": "Pennsylvania", "head_coach": "Jay Vidovich", "recruiting_email": "msoccer@athletics.pitt.edu", "camp_dates": "TBD"},
+    {"name": "SMU", "conference": "ACC", "state": "Texas", "head_coach": "Kevin Hudson", "recruiting_email": "khudson@smu.edu", "camp_dates": "TBD"},
+    {"name": "Stanford University", "conference": "ACC", "state": "California", "head_coach": "Jeremy Gunn", "recruiting_email": "smsoccer@stanford.edu", "camp_dates": "TBD"},
+    {"name": "Virginia Tech", "conference": "ACC", "state": "Virginia", "head_coach": "Mike Brizendine", "recruiting_email": "vtmsoccer@vt.edu", "camp_dates": "TBD"},
 
     # Big East
     {"name": "Georgetown University", "conference": "Big East", "state": "Washington DC", "head_coach": "Brian Wiese", "recruiting_email": "gumenssoccer@georgetown.edu", "camp_dates": "TBD"},
     {"name": "Creighton University", "conference": "Big East", "state": "Nebraska", "head_coach": "Johnny Torres", "recruiting_email": "creightonsocceracademy@creighton.edu", "camp_dates": "TBD"},
     {"name": "St. John's University", "conference": "Big East", "state": "New York", "head_coach": "Dr. David Masur", "recruiting_email": "masurd@stjohns.edu", "camp_dates": "TBD"},
     {"name": "University of Connecticut", "conference": "Big East", "state": "Connecticut", "head_coach": "Chris Gbandi", "recruiting_email": "menssoccer@uconn.edu", "camp_dates": "TBD"},
+    {"name": "University of Akron", "conference": "Big East", "state": "Ohio", "head_coach": "Jared Embick", "recruiting_email": "menssoccer@uakron.edu", "camp_dates": "TBD"},
+    {"name": "Butler University", "conference": "Big East", "state": "Indiana", "head_coach": "Ian Sarachan", "recruiting_email": "isarachan@butler.edu", "camp_dates": "TBD"},
+    {"name": "DePaul University", "conference": "Big East", "state": "Illinois", "head_coach": "Mark Plotkin", "recruiting_email": "mplotkin@depaul.edu", "camp_dates": "TBD"},
+    {"name": "Marquette University", "conference": "Big East", "state": "Wisconsin", "head_coach": "David Korn", "recruiting_email": "TBD", "camp_dates": "TBD"},
+    {"name": "Providence College", "conference": "Big East", "state": "Rhode Island", "head_coach": "Craig Stewart", "recruiting_email": "CSTEWAR3@PROVIDENCE.EDU", "camp_dates": "TBD"},
+    {"name": "Seton Hall University", "conference": "Big East", "state": "New Jersey", "head_coach": "Andreas Lindberg", "recruiting_email": "TBD", "camp_dates": "TBD"},
+    {"name": "Villanova University", "conference": "Big East", "state": "Pennsylvania", "head_coach": "Mark Fetrow", "recruiting_email": "TBD", "camp_dates": "TBD"},
+    {"name": "Xavier University", "conference": "Big East", "state": "Ohio", "head_coach": "John Higgins", "recruiting_email": "higginsj5@xavier.edu", "camp_dates": "TBD"},
 
     # Big Ten
     {"name": "Northwestern University", "conference": "Big Ten", "state": "Illinois", "head_coach": "Russell Payne", "recruiting_email": "msoccer@northwestern.edu", "camp_dates": "TBD"},
     {"name": "University of Michigan", "conference": "Big Ten", "state": "Michigan", "head_coach": "Chaka Daley", "recruiting_email": "email.mscoccer@umich.edu", "camp_dates": "TBD"},
     {"name": "Penn State University", "conference": "Big Ten", "state": "Pennsylvania", "head_coach": "Rob Dow", "recruiting_email": "msoc@athletics.psu.edu", "camp_dates": "TBD"},
     {"name": "University of Maryland", "conference": "Big Ten", "state": "Maryland", "head_coach": "Sasho Cirovski", "recruiting_email": "sasho@umd.edu", "camp_dates": "TBD"},
+    {"name": "Indiana University", "conference": "Big Ten", "state": "Indiana", "head_coach": "Todd Yeagley", "recruiting_email": "TBD", "camp_dates": "TBD"},
+    {"name": "Michigan State University", "conference": "Big Ten", "state": "Michigan", "head_coach": "Damon Rensing", "recruiting_email": "rensingd@ath.msu.edu", "camp_dates": "TBD"},
+    {"name": "Ohio State University", "conference": "Big Ten", "state": "Ohio", "head_coach": "Brian Maisonneuve", "recruiting_email": "maisonneuve.2@osu.edu", "camp_dates": "TBD"},
+    {"name": "Rutgers University", "conference": "Big Ten", "state": "New Jersey", "head_coach": "Jim McElderry", "recruiting_email": "TBD", "camp_dates": "TBD"},
+    {"name": "UCLA", "conference": "Big Ten", "state": "California", "head_coach": "Ryan Jorden", "recruiting_email": "msoccer@athletics.ucla.edu", "camp_dates": "TBD"},
+    {"name": "University of Washington", "conference": "Big Ten", "state": "Washington", "head_coach": "Jamie Clark", "recruiting_email": "huskysoccer@uw.edu", "camp_dates": "TBD"},
+    {"name": "University of Wisconsin", "conference": "Big Ten", "state": "Wisconsin", "head_coach": "Neil Jones", "recruiting_email": "wisconsinmsoc@athletics.wisc.edu", "camp_dates": "TBD"},
 
-    # Others
+    # AAC
     {"name": "Rice University", "conference": "AAC", "state": "Texas", "head_coach": "TBD", "recruiting_email": "TBD", "camp_dates": "TBD"},
-    {"name": "SMU", "conference": "AAC", "state": "Texas", "head_coach": "TBD", "recruiting_email": "TBD", "camp_dates": "TBD"},
-    {"name": "Carnegie Mellon University", "conference": "UAA", "state": "Pennsylvania", "head_coach": "TBD", "recruiting_email": "TBD", "camp_dates": "TBD"},
-    {"name": "Johns Hopkins University", "conference": "Centennial", "state": "Maryland", "head_coach": "TBD", "recruiting_email": "TBD", "camp_dates": "TBD"},
-    {"name": "Tufts University", "conference": "NESCAC", "state": "Massachusetts", "head_coach": "TBD", "recruiting_email": "TBD", "camp_dates": "TBD"},
 ]
 
 with app.app_context():
